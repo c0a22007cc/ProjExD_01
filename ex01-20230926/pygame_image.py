aebd5d27ea4e85df0b1d7e2ac2220d68d4ab398c
anzai <c0a22007cc@edu.teu.ac.jp>
2023-09-26 15:27:25 +0900

    ex01-2

--- ex01-20230926/pygame_image.py
+++ ex01-20230926/pygame_image.py
@@ -7,8 +7,9 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/ex01-20230926/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex01/ex01-20230926/fig/3.jpg")
+    kk_img = pg.image.load("ex01/ex01-20230926/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+
     tmr = 0
     while True:
         for event in pg.event.get():
