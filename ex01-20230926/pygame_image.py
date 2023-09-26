60e711403f7a2a61ce265826e186e508a08ab805
anzai <c0a22007cc@edu.teu.ac.jp>
2023-09-26 16:23:37 +0900

    ex01-6

--- ex01-20230926/pygame_image.py
+++ ex01-20230926/pygame_image.py
@@ -10,20 +10,20 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("ex01/ex01-20230926/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_img_rzoom = pg.transform.rotozoom(kk_img, 10, 1.0)
-    kk_imgs = [kk_img, kk_img_rzoom]
+    kk_img_rz1 = pg.transform.rotozoom(kk_img, 10, 1.0)
+    kk_imgs = [kk_img, kk_img_rz1]
 
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         x = tmr % 1600
-        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [- x, 0])
         screen.blit(bg_img, [1600 - x, 0])
         screen.blit(kk_imgs[tmr%2], [300, 200])#tmrの値が奇数または偶数
         pg.display.update()
         tmr += 1        
-        clock.tick(150)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
